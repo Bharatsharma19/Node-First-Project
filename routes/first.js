164cf712f80a98888c2a9b1b5d7b46a1534676f6
Bharat Sharma <82795831+Bharatsharma19@users.noreply.github.com>
2022-08-01 17:49:58 +0530

    01 Aug 2022

--- routes/first.js
+++ routes/first.js
@@ -2,12 +2,11 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/first", function (req, res) {
-  res.render(
-    "first",
-    { name: "Bharat Sharma" },
-    { desg: "Software Developer" },
-    { salary: 1024659796 }
-  );
+  res.render("first", {
+    name: "Bharat Sharma",
+    desg: "Software Developer",
+    salary: 1024659796,
+  });
 });
 
 module.exports = router;
